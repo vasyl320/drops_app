50e29ecc03662bb372c55362232c2803e8bbd60e
vasyl320 <vasilio7levvasik@gmail.com>
2026-01-18 09:37:39 +0100

    Initial Commit

---
+++ drops_app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  drops_app
+//
+//  Created by ta636 on 18.01.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ drops_app/drops_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  drops_appApp.swift
+//  drops_app
+//
+//  Created by ta636 on 18.01.26.
+//
+
+import SwiftUI
+
+@main
+struct drops_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
