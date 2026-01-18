6b963381b115c41f255911a842631d3f67e71733
vasyl320 <vasilio7levvasik@gmail.com>
2026-01-18 15:00:10 +0100

    zahlenänderung

--- drops_app/DropsApp.swift
+++ drops_app/DropsApp.swift
@@ -1,17 +1,15 @@
-//
-//  drops_appApp.swift
-//  drops_app
-//
-//  Created by ta636 on 18.01.26.
-//
-
 import SwiftUI
 
 @main
-struct drops_appApp: App {
+struct DropsApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
 }
+
+#Preview {
+    ContentView()
+}
+
